dff09405440b883b99374827cd0c3c81dfb5e027
JP <jinardent1@likelion.org>
2019-11-07 08:17:31 +0000

    another commit

--- ting/app/controllers/posts_controller.rb
+++ ting/app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :avatar)
+      params.require(:post).permit(:title, :content, :sex, :avatar)
     end
 end
